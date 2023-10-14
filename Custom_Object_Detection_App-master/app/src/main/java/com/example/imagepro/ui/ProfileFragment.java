402f062b163a7b517827bd5a40554751274134cc
ZakariaHossain56 <zakahossain22@gmail.com>
2023-10-15 03:09:03 +0600

    profile fragment

--- Custom_Object_Detection_App-master/app/src/main/java/com/example/imagepro/ui/ProfileFragment.java
+++ Custom_Object_Detection_App-master/app/src/main/java/com/example/imagepro/ui/ProfileFragment.java
@@ -24,9 +24,7 @@ public class ProfileFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        textView = view.findViewById(R.id.text_account);
 
-        textView.setText("Profile fragment");
 
     }
 
