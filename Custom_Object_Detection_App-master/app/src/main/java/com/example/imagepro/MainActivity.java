c9f1c0fbeae3c80c8e92d7cb47f445a93c004475
1907008 Abrar <113090760+MahirAbrar3425@users.noreply.github.com>
2023-11-06 15:17:56 +0600

    text to sign

--- Custom_Object_Detection_App-master/app/src/main/java/com/example/imagepro/MainActivity.java
+++ Custom_Object_Detection_App-master/app/src/main/java/com/example/imagepro/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.imagepro;
 
+import static android.content.ContentValues.TAG;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
@@ -10,7 +13,10 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -19,10 +25,17 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.navigation.NavigationView;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.ListResult;
+import com.google.firebase.storage.StorageReference;
 
 import org.opencv.android.OpenCVLoader;
 
+import java.io.File;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,9 +43,15 @@ public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
+
+
     private DrawerLayout drawerLayout;
     private NavigationView navView;
     private NavController navController;
+    boolean downloadSuccessful,downloadUpdate,colorChange = false;
+
+    private static final String PREFS_NAME = "MyPrefsFile";
+    private static final String FIRST_TIME_KEY = "isFirstTime";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,11 +68,12 @@ public class MainActivity extends AppCompatActivity {
 
         drawerLayout = findViewById(R.id.drawer_layout);
         navView = findViewById(R.id.nav_view);
-
+        colorChange=downloadUpdates(false);
+        showToast("colorchange  "+colorChange);
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home,R.id.nav_profile, R.id.nav_how_to_use, R.id.nav_about_sign_language,
                 R.id.nav_history,R.id.nav_aboutus,R.id.nav_terms,R.id.nav_contactus,R.id.nav_privacy,
-                R.id.nav_settings,
+                R.id.nav_settings,R.id.nav_update,
                 R.id.signout)
                 .setOpenableLayout(drawerLayout)
                 .build();
@@ -61,6 +81,32 @@ public class MainActivity extends AppCompatActivity {
         navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+        /*if(colorChange)
+        {
+            NavigationView navigationView = findViewById(R.id.nav_view);
+            Menu menu = navigationView.getMenu();
+           // menu.findItem(R.id.nav_home).setIconTintList(ContextCompat.getColorStateList(this, R.color.black));
+            //menu.findItem(R.id.nav_home).set(ContextCompat.getColorStateList(this, R.color.your_custom_color));
+            menu.findItem(R.id.nav_update).setTitleCondensed("Download Upadtes");
+            int iid = R.id.nav_update;
+            //showToast("befbbe");
+        }*/
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+
+        // Check if the app is launched for the first time
+        boolean isFirstTime = settings.getBoolean(FIRST_TIME_KEY, true);
+
+        if (isFirstTime) {
+            // Perform initialization for the first launch
+            SharedPreferences SP = getSharedPreferences("SP", MODE_PRIVATE);
+            SharedPreferences.Editor editor = SP.edit();
+            int initialCount = 0;
+            editor.putInt("putStringCount", initialCount).apply();
+            // Update the flag to indicate that the app has been launched before
+            SharedPreferences.Editor editor1 = settings.edit();
+            editor1.putBoolean(FIRST_TIME_KEY, false);
+            editor1.apply();
+        }
 
         navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -72,6 +118,14 @@ public class MainActivity extends AppCompatActivity {
                 else if(item.getItemId() == R.id.signout){
                     signOutUser();
                     return false;
+                }
+                else if(item.getItemId() == R.id.nav_update)
+                {
+
+                    downloadUpdates(true);
+                    NavigationView navigationView = findViewById(R.id.nav_view);
+                    Menu menu = navigationView.getMenu();
+                    menu.findItem(R.id.nav_update).setVisible(false);
                 }
 
                 int id = item.getItemId();
@@ -100,9 +154,149 @@ public class MainActivity extends AppCompatActivity {
         showToast("sign out clicked");
     }
 
+    private boolean downloadUpdates(boolean isDownload){
+        downloadUpdate = false;
+        StorageReference storageRef = FirebaseStorage.getInstance().getReference("videos/");
+        getTotalFileCount(isDownload,"videos",new OnSuccessListener<Long>() {
+            @Override
+            public void onSuccess(Long totalFiles) {
+                // Do something with the total number of files
+                if(!isDownload)
+                {
+                    SharedPreferences SP = getSharedPreferences("SP", MODE_PRIVATE);
+                    SharedPreferences.Editor editor = SP.edit();
+                    int currentCount = SP.getInt("putStringCount", 0);
+                    showToast("currentCount "+currentCount+" totalCount "+totalFiles);
+                    if(totalFiles > currentCount)
+                    {
+                        downloadUpdate = true;
+                        NavigationView navigationView = findViewById(R.id.nav_view);
+                        Menu menu = navigationView.getMenu();
+                        // menu.findItem(R.id.nav_home).setIconTintList(ContextCompat.getColorStateList(this, R.color.black));
+                        //menu.findItem(R.id.nav_home).set(ContextCompat.getColorStateList(this, R.color.your_custom_color));
+                        menu.findItem(R.id.nav_update).setVisible(true);
 
 
+                        //showToast("byfbeubo");
+                        Context context = getApplicationContext();
+                        Long newWord = totalFiles-currentCount;
+                        NotificationHelper.showNotification(context, "Update Available", "New "+newWord+" sign words available.");
+                    }
+                }
 
+
+// Increment the counter
+                //int cnt = 1;
+
+                //editor.putInt("putStringCount", currentCount + 1);
+            }
+        });
+        return downloadUpdate;
+    }
+
+     public void getTotalFileCount(boolean isDownload , String storagePath, final OnSuccessListener<Long> successListener) {
+         FirebaseStorage storage = FirebaseStorage.getInstance();
+         StorageReference storageRef = storage.getReference().child(storagePath);
+
+         storageRef.listAll()
+                .addOnSuccessListener(new OnSuccessListener<ListResult>() {
+                    @Override
+                    public void onSuccess(ListResult listResult) {
+                        // Get the total number of files
+                        long totalFiles = listResult.getItems().size();
+
+                        // Callback with the total number of files
+                        successListener.onSuccess(totalFiles);
+
+                            SharedPreferences SP = getSharedPreferences("SP", MODE_PRIVATE);
+                            int currentCount = SP.getInt("putStringCount", 0);
+                            int cnt = 0;
+                            SharedPreferences.Editor editor = SP.edit();
+if(isDownload)
+{
+    for (StorageReference fileRef : listResult.getItems()) {
+        if(currentCount > cnt)
+        {
+            cnt=cnt+1;
+            // editor.putInt("putStringCount", cnt+1).apply();
+            //continue;
+        }
+        //cnt = SP.getInt("putStringCount",0);
+        if(downloadFile(fileRef)) {
+            //cnt = cnt + 1;
+        }
+        // editor.putInt("putStringCount", initialCount).apply()
+
+    }
+}
+
+                            //cnt = cnt - 1;
+                            //editor.putInt("putStringCount", cnt).apply();
+
+
+                    }
+                })
+                .addOnFailureListener(exception -> {
+                    // Handle errors
+                    exception.printStackTrace();
+                });
+    }
+    public boolean downloadFile(StorageReference storageRef)
+    {
+         downloadSuccessful = false;
+        File localFile;
+        String fileName = storageRef.getName();
+        SharedPreferences videoRef = getSharedPreferences("videoRef",MODE_PRIVATE);
+       // String path = videoRef.getString(fileName+".mp4",null);
+        if (!videoRef.contains(fileName+".mp4")) {
+            try {
+                String dirName = "MyCustomDir";
+                Context c = getApplicationContext();
+                ContextWrapper cw = new ContextWrapper(c);
+                File customDir = cw.getDir(dirName, MODE_PRIVATE);
+                SharedPreferences SP = getSharedPreferences("SP", MODE_PRIVATE);
+                int currentCount = SP.getInt("putStringCount", 0);
+                localFile = File
+                        .createTempFile(fileName, "mp4",customDir);
+                // Log.i(TAG, localFile.getAbsolutePath());
+                storageRef.getFile(localFile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        SharedPreferences videoRef = getSharedPreferences("videoRef",MODE_PRIVATE);
+                        SharedPreferences.Editor editor = videoRef.edit();
+                        editor.putString(fileName,localFile.getAbsolutePath());
+                        editor.apply();
+
+                        Toast.makeText(MainActivity.this, "Downloaded "+fileName+" "+localFile.getAbsolutePath(), Toast.LENGTH_LONG).show();
+                        downloadSuccessful = true;
+                        SharedPreferences SP = getSharedPreferences("SP", MODE_PRIVATE);
+                        int currentCount = SP.getInt("putStringCount", 0);
+
+
+                        //int cnt = 0;
+                        SharedPreferences.Editor editorD = SP.edit();
+                        editorD.putInt("putStringCount", currentCount+1).apply();
+
+                    }
+
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        // Handle any errors
+                        Toast.makeText(MainActivity.this, "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                    }
+                });
+            } catch (IOException e) {
+                Log.e(TAG, e.toString());
+                e.printStackTrace();
+                Toast.makeText(MainActivity.this, "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
+
+            }
+        }
+
+        return downloadSuccessful;
+    }
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         int id = item.getItemId();
--- Custom_Object_Detection_App-master/app/src/main/java/com/example/imagepro/NotificationHelper.java
+++ Custom_Object_Detection_App-master/app/src/main/java/com/example/imagepro/NotificationHelper.java
@@ -1,2 +1,40 @@
-package com.example.imagepro;public class NotificationHelper {
+package com.example.imagepro;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Build;
+
+import androidx.core.app.NotificationCompat;
+
+public class NotificationHelper {
+
+    private static final String CHANNEL_ID = "my_channel";
+    private static final CharSequence CHANNEL_NAME = "My Channel";
+    private static final String CHANNEL_DESCRIPTION = "Description for My Channel";
+
+    public static void showNotification(Context context, String title, String message) {
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        // Create a notification channel for devices running Android 8.0 (API level 26) and higher
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(CHANNEL_DESCRIPTION);
+            channel.enableLights(true);
+            channel.setLightColor(Color.RED);
+            channel.enableVibration(true);
+            notificationManager.createNotificationChannel(channel);
+        }
+
+        // Create a notification
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.ic_dialog_info)
+                .setContentTitle(title)
+                .setContentText(message)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        // Show the notification
+        notificationManager.notify(0, builder.build());
+    }
 }
